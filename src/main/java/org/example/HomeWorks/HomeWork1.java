10fc099c1cf7de45d17dc86ed8ac332bd154fb24
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-09-21 18:38:27 +0300

    HW1

---
+++ src/main/java/org/example/HomeWorks/HomeWork1.java
@@ -0,0 +1,14 @@
+package org.example.HomeWorks;
+
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class HomeWork1 {
+    public static double average(List<Double> list) {
+        return list.stream()
+                .filter(n -> n.doubleValue() % 2 == 0)
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
+    }
+}
