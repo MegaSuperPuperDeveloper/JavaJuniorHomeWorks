3d3305b6a9d24d2f07a5ad7305a3d08bc8c5db7d
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-09-22 13:16:32 +0300

    HW1

--- src/main/java/org/example/HomeWorks/HomeWork1.java
+++ src/main/java/org/example/HomeWorks/HomeWork1.java
@@ -6,7 +6,7 @@ import java.util.OptionalDouble;
 public class HomeWork1 {
     public static double average(List<Double> list) {
         return list.stream()
-                .filter(n -> n.doubleValue() % 2 == 0)
+                .filter(n -> n % 2 == 0)
                 .mapToDouble(Double::doubleValue)
                 .average()
                 .orElse(0.0);
